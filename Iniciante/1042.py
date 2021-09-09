d5cd9bd5f39da6651ca0416c05bd20c539100482
Marcelo Soares <marcelotc1998@gmail.com>
2021-09-09 13:48:40 -0300

    Exercício adicionado

--- Iniciante/1042.py
+++ Iniciante/1042.py
@@ -1,3 +1,22 @@
-def classifica_numeros(numeros: list):
-    for i in range(len(numeros)):
-        if i < len(numeros) - 1:
+def ordena_numeros(numeros: list):
+    for i in range(1, len(numeros)):
+        posicaoAnterior = i - 1
+        while posicaoAnterior >= 0:
+            posicaoAtual = posicaoAnterior + 1
+            if numeros[posicaoAtual] < numeros[posicaoAnterior]:
+                numeros[posicaoAnterior], numeros[posicaoAtual] = numeros[posicaoAtual], numeros[posicaoAnterior]
+            else:
+                break
+            posicaoAnterior -= 1
+
+
+if __name__ == "__main__":
+    entradas = [int(entrada) for entrada in input().split()]
+    entradasOriginal = tuple(entradas)
+    ordena_numeros(entradas)
+    for num in entradas:
+        print(num)
+    print()
+    for num in entradasOriginal:
+        print(num)
+
